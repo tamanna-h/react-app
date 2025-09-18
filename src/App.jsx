55c8486e5574df5c0754df0df0ef3a557da4d5c8
tahmina tamanna <tahminadox@gmail.com>
2025-09-18 06:56:29 +0600

    API fetched and showed in console

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Search from "./components/Search";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
   method: "GET",
   headers: {
@@ -19,12 +19,20 @@ function App() {
   const fetchMovies = async () => {
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
       const response = await fetch(endpoint, API_OPTIONS);
-      // alert(response);
-      // throw new Error('Failed to fetch movies');
-    } catch (error) {
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch movies`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    }
+    catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage(`Error fetching movies, please try again later ${error}`);
+      
     }
   };
 
